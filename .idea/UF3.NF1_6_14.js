0cf0f76c2e43b4c8a6f7b9015eaa9aa2697daab7
mariia22022 <6057721@gmail.com>
2022-10-31 12:55:19 +0100

    commit 1

---
+++ .idea/UF3.NF1_6_14.js
@@ -0,0 +1,53 @@
+/* exercicio 6*/
+console.log('exercicio 6')
+let number = 1234;
+let  biginter = 5768n;
+console.log( number + Number(biginter));
+console.log(number - Number(biginter));
+console.log(number * Number(biginter));
+console.log((number / Number(biginter)).toFixed(1));
+
+
+/* exercicio 7*/
+console.log('exercicio 7')
+let catet = 4;
+let hipotenusa = (2*catet*catet)**(1/2);
+console.log(hipotenusa.toFixed(2))
+
+/* exercicio 8*/
+console.log('exercicio 8')
+let num= 1234;
+let d = 'abcd';
+console.log(num/d)
+
+/* exercicio 9*/
+console.log('exercicio 9')
+let name= 'Mariia Levina'
+console.log(name.slice(7,13))
+
+
+/* exercicio 10*/
+console.log('exercicio 10')
+let name1= 'Mariia Levina'
+console.log(name1.slice(0,5)+name1.slice(6,13))
+
+/* exercicio 11*/
+console.log('exercicio 11')
+let a =NULL;
+let b= undefined;
+console.log(a+b);
+
+/* exercicio 12*/
+console.log('exercicio 12')
+console.log(String(false))
+console.log(false+'')
+
+/* exercicio 13*/
+console.log('exercicio 13')
+console.log(Boolean('dfgadfg'))
+console.log(2=='string')
+
+/* exercicio 14*/
+console.log('exercicio 14')
+console.log(Number(true))
+console.log(6==7)
